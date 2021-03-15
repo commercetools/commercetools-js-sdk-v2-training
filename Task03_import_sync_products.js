89f47581752e2331c4b0888fa4a7121031d08881
Fady Fawzy <fady.fawzy19@hotmail.com>
2021-03-15 13:03:04 +0100

    exercise fixes

--- Task03_import_sync_products.js
+++ Task03_import_sync_products.js
@@ -13,14 +13,10 @@ checkImportOperationStatus("productsImporterAndSync","bd71c221-e799-4d2c-bc9d-b6
 
 //https://github.com/commercetools/commercetools-project-sync#run
 // docker run \
-// -e SOURCE_AUTH_URL=https://auth.europe-west1.gcp.commercetools.com \ 
-// -e SOURCE_API_URL=https://api.europe-west1.gcp.commercetools.com \
-// -e SOURCE_PROJECT_KEY=happy-garden-dev-jan11 \
-// -e SOURCE_CLIENT_ID=ynRYE7WbSf_u9KXCjm_dS7SD \
-// -e SOURCE_CLIENT_SECRET=SYFGk4ZB4R6u-Ytmh60eeAbZj-aEZzi1 \
-// -e TARGET_PROJECT_KEY=happy-garden-test-jan11 \
-// -e TARGET_CLIENT_ID=8u9Waf_J-KRHIG3J9D2bgeNG \
-// -e TARGET_CLIENT_SECRET=iN4P21ucP0ZNlaEdXXkGxARYxVhDhXft \
-// -e TARGET_AUTH_URL=https://auth.europe-west1.gcp.commercetools.com \
-// -e TARGET_API_URL=https://api.europe-west1.gcp.commercetools.com \
+// -e SOURCE_PROJECT_KEY= \
+// -e SOURCE_CLIENT_ID= \
+// -e SOURCE_CLIENT_SECRET= \
+// -e TARGET_PROJECT_KEY= \
+// -e TARGET_CLIENT_ID= \
+// -e TARGET_CLIENT_SECRET= \
 // commercetools/commercetools-project-sync:3.10.1 -s all
